08c80d30316939ac593ea1be24ebeceae3c0d5f3
gitiboys <82661142+gitiboys@users.noreply.github.com>
2021-05-06 12:44:12 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -21,6 +21,7 @@ else:
 from translation import Translation
 
 import pyrogram
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton,
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 from helper_funcs.chat_base import TRChatBase
